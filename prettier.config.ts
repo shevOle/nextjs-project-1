0215e7654adc2f104d4ea784d5b23c51113c1a43
Oleg Shevchenko <72355488+Am4rel@users.noreply.github.com>
2025-10-06 19:02:30 +0300

    feat: add project setup files

---
+++ prettier.config.ts
@@ -0,0 +1,10 @@
+import { type Config } from 'prettier';
+
+const config: Config = {
+  trailingComma: 'es5',
+  singleQuote: true,
+  tabWidth: 2,
+  jsxSingleQuote: true,
+};
+
+export default config;
